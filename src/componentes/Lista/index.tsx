51a0e4bd134a5ba8ffdace9ec436094dd679a1ca
AmandaPVSantos <169503404+AmandaPVSantos@users.noreply.github.com.>
2024-12-19 15:04:47 -0300

    reaproveitando interfaces

--- src/componentes/Lista/index.tsx
+++ src/componentes/Lista/index.tsx
@@ -3,14 +3,20 @@ import Item from './item';
 import Style from './Lista.module.scss';
 import { ITarefa } from '../../types/tarefa';
 
-function Lista({tarefas}: {tarefas: ITarefa[]}) {
+interface Props {
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void // void pois nao retornar nenum valor
+}
+
+function Lista({tarefas, selecionaTarefa}: Props) {
     return(
         <aside className={Style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
-                {tarefas.map((item, index) => (
+                {tarefas.map(item => (
                     <Item 
-                    key={index}
+                    selecionaTarefa={selecionaTarefa}
+                    key={item.id}
                     {...item}
                     />    
                 ))} 
--- src/componentes/Lista/item/index.tsx
+++ src/componentes/Lista/item/index.tsx
@@ -2,10 +2,36 @@ import React from 'react';
 import Style from '../Lista.module.scss';
 import { ITarefa } from '../../../types/tarefa';
 
-export default function Item({tarefa, tempo, selecionado, completado, id}: ITarefa) {
+// Estende uma interface para outra,
+// permitindo a comunicação de informações entre interfaces.
+interface Props extends ITarefa {
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+
+export default function Item(
+    {
+        tarefa, 
+        tempo, 
+        selecionado, 
+        completado, 
+        id, 
+        selecionaTarefa
+    }: Props) {
     console.log('item atual: ', {tarefa, tempo, selecionado, completado, id});
     return (
-        <li className={Style.item}>
+        <li 
+            className={Style.item} 
+            onClick={() => selecionaTarefa(
+                {
+                    tarefa,
+                    tempo,
+                    selecionado,
+                    completado,
+                    id
+                }
+            )}
+        >
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
         </li>
